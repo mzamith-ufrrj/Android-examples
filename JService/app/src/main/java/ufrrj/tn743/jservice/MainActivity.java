ce13d906c57039e1563b0f65917d903c313ea068
Marcelo <mzamith@ufrrj.br>
2022-11-15 15:07:17 -0300

    update 2022-11-15 #1

---
+++ JService/app/src/main/java/ufrrj/tn743/jservice/MainActivity.java
@@ -0,0 +1,14 @@
+    package ufrrj.tn743.jservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
